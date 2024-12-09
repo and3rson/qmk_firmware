26bf6da0cf711556b2292e601f30ebd9c594574f
Andrew Dunai <a@dun.ai>
2024-12-09 21:03:49 +0200

    Fix crkbd build

--- keyboards/crkbd/keymaps/anderson/rules.mk
+++ keyboards/crkbd/keymaps/anderson/rules.mk
@@ -1,5 +1,5 @@
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_DRIVER     = ssd1306
 LTO_ENABLE      = yes
 # RGBLIGHT_ENABLE = no
 EXTRAKEY_ENABLE = yes   # Audio control and System control
